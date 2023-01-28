72a2893ee6c6221c4b2c22f99697c7447b391135
MichaelRodionov <106465054+MichaelRodionov@users.noreply.github.com>
2023-01-28 20:50:58 +0300

    fix bugs

--- application/views/arena.py
+++ application/views/arena.py
@@ -1,70 +1,70 @@
-from flask import Blueprint, render_template
-
-from application.views.warriors import heroes
-from implemented import arena
-
-
-# ----------------------------------------------------------------
-# initialize arena blueprint
-arena_blueprint = Blueprint('arena_blueprint', __name__, template_folder='templates')
-
-
-# ----------------------------------------------------------------
-# arena views
-@arena_blueprint.route("/fight/")
-def start_fight():
-    """
-    View to start game and render fight page
-    :return: rendered fight page
-    """
-    arena.start_game(player=heroes['player'], enemy=heroes['enemy'])
-    return render_template('fight.html', title='fight', heroes=heroes)
-
-
-@arena_blueprint.route("/fight/hit")
-def hit():
-    """
-    view to hit enemy and render fight page
-    :return: rendered fight page
-    """
-    if arena.game_is_running:
-        result: str = arena.player_hit()
-    else:
-        result: str = arena.battle_result
-
-    return render_template('fight.html', heroes=heroes, result=result)
-
-
-@arena_blueprint.route("/fight/use-skill")
-def use_skill():
-    """
-    View to use a skill and render the fight page
-    :return: rendered fight page
-    """
-    if arena.game_is_running:
-        result: str = arena.player_use_skill()
-    else:
-        result: str = arena.battle_result
-    return render_template('fight.html', heroes=heroes, result=result)
-
-
-@arena_blueprint.route("/fight/pass-turn")
-def pass_turn():
-    """
-    View to pas turn and render the fight page
-    :return: rendered fight page
-    """
-    if arena.game_is_running:
-        result: str = arena.next_turn()
-    else:
-        result: str = arena.battle_result
-    return render_template('fight.html', heroes=heroes, result=result)
-
-
-@arena_blueprint.route("/fight/end-fight")
-def end_fight():
-    """
-    View to end the battle
-    :return: rendered main page
-    """
-    return render_template("index.html", heroes=heroes)
+# from flask import Blueprint, render_template
+#
+# from application.views.warriors import heroes
+# from implemented import arena
+#
+#
+# # ----------------------------------------------------------------
+# # initialize arena blueprint
+# arena_blueprint = Blueprint('arena_blueprint', __name__, template_folder='templates')
+#
+#
+# # ----------------------------------------------------------------
+# # arena views
+# @arena_blueprint.route("/fight/")
+# def start_fight():
+#     """
+#     View to start game and render fight page
+#     :return: rendered fight page
+#     """
+#     arena.start_game(player=heroes['player'], enemy=heroes['enemy'])
+#     return render_template('fight.html', title='fight', heroes=heroes)
+#
+#
+# @arena_blueprint.route("/fight/hit")
+# def hit():
+#     """
+#     view to hit enemy and render fight page
+#     :return: rendered fight page
+#     """
+#     if arena.game_is_running:
+#         result: str = arena.player_hit()
+#     else:
+#         result: str = arena.battle_result
+#
+#     return render_template('fight.html', heroes=heroes, result=result)
+#
+#
+# @arena_blueprint.route("/fight/use-skill")
+# def use_skill():
+#     """
+#     View to use a skill and render the fight page
+#     :return: rendered fight page
+#     """
+#     if arena.game_is_running:
+#         result: str = arena.player_use_skill()
+#     else:
+#         result: str = arena.battle_result
+#     return render_template('fight.html', heroes=heroes, result=result)
+#
+#
+# @arena_blueprint.route("/fight/pass-turn")
+# def pass_turn():
+#     """
+#     View to pas turn and render the fight page
+#     :return: rendered fight page
+#     """
+#     if arena.game_is_running:
+#         result: str = arena.next_turn()
+#     else:
+#         result: str = arena.battle_result
+#     return render_template('fight.html', heroes=heroes, result=result)
+#
+#
+# @arena_blueprint.route("/fight/end-fight")
+# def end_fight():
+#     """
+#     View to end the battle
+#     :return: rendered main page
+#     """
+#     return render_template("index.html", heroes=heroes)
--- application/views/menu.py
+++ application/views/menu.py
@@ -1,16 +1,16 @@
-from flask import Blueprint, render_template
-
-
-# ----------------------------------------------------------------
-# initialize blueprint
-menu_blueprint = Blueprint('menu_blueprint', __name__, template_folder='templates')
-
-
-# ----------------------------------------------------------------
-@menu_blueprint.route("/")
-def menu_page():
-    """
-    Main menu view
-    :return: main page
-    """
-    return render_template('index.html', title='main menu')
+# from flask import Blueprint, render_template
+#
+#
+# # ----------------------------------------------------------------
+# # initialize blueprint
+# menu_blueprint = Blueprint('menu_blueprint', __name__, template_folder='templates')
+#
+#
+# # ----------------------------------------------------------------
+# @menu_blueprint.route("/")
+# def menu_page():
+#     """
+#     Main menu view
+#     :return: main page
+#     """
+#     return render_template('index.html', title='main menu')
